ef3f15610c6523e02349fe0c292d111ad050702d
treciaflordespanso-netizen <treciaflor.despanso@csucc.edu.ph>
2026-09-24 19:19:40 +0800

    "Add the complete function"

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -16,18 +16,18 @@ void main() {
   do {
     print('\n--- Round $roundNumber ---');
 
-    // Player 1 enters a move.
+    // Player 1 mag enter sa iyang move
     String playerOneMove = getMove(playerOne);
 
-    // Print about 30 blank lines to hide Player 1's move.
+    // Mag print og 30 ka empty lines para di makita ang input sa isa ka player
     for (int i = 0; i < 30; i++) {
       print('');
     }
 
-    // Player 2 enters a move.
+    // Player 2 mag enter sa iyang move
     String playerTwoMove = getMove(playerTwo);
 
-    // Decide the winner of the round.
+    // Decide kung kinsa ang daog sa round.
     String? winner = decideWinner(playerOneMove, playerTwoMove);
 
     print('\n$playerOne chose $playerOneMove. '
@@ -47,7 +47,7 @@ void main() {
       'Score -> $playerOne: $playerOneScore | $playerTwo: $playerTwoScore',
     );
 
-    // Ask if the players want to play another round.
+    // Ask kung ang players gusto pa mag dula og another round
     while (true) {
       stdout.write('Play again? (y/n): ');
 
@@ -81,7 +81,7 @@ void main() {
   }
 }
 
-/// Gets a player's name and uses a default name if the input is empty or null.
+/// kuhaon niya ang player name nya og wala naay naka set na name para sa ana nga player
 String getPlayerName(int playerNumber) {
   stdout.write('Enter Player $playerNumber name: ');
 
@@ -99,7 +99,7 @@ String getPlayerName(int playerNumber) {
   return playerName;
 }
 
-/// Get's a player's move and keep asking until a valid move is entered
+/// Kuhaon ang move sa player ug mag sige og pangutana hangtod sa makahatag siya og valid nga move
 String getMove(String playerName) {
   while (true) {
     stdout.write(
@@ -119,7 +119,7 @@ String getMove(String playerName) {
   }
 }
 
-/// Checks whether a move is valid and returns it in lowercasee.
+/// e check niya ang move na gi pili ni player og e return niya in lowercase
 String? validateMove(String? input) {
   List<String> validMoves = [
     'rock',
@@ -139,7 +139,7 @@ if (validMoves.contains(move)) {
 return null;
 }
 
-/// Decides the winner based on two player's moves
+/// Mao ang mag decide sa winner base sa gi pili na move
 String? decideWinner(String playerOneMove, String playerTwoMove) {
   if (playerOneMove == playerTwoMove) {
     return null;
